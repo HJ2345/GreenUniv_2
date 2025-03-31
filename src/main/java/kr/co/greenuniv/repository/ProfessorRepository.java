200b3841f8498a17eed4201f5cd1259a32105b9c
HJ <wnsgur0028@gmail.com>
2025-03-31 17:52:10 +0900

    ver1.1

--- src/main/java/kr/co/greenuniv/repository/ProfessorRepository.java
+++ src/main/java/kr/co/greenuniv/repository/ProfessorRepository.java
@@ -2,7 +2,9 @@ package kr.co.greenuniv.repository;
 
 import kr.co.greenuniv.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 
 }

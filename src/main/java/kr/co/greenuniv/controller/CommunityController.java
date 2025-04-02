82e52d59d0ed34f9226cbd4318f82885fee9655d
HJ <wnsgur0028@gmail.com>
2025-04-02 17:07:57 +0900

    ver0.0 3 snapshot

--- src/main/java/kr/co/greenuniv/controller/CommunityController.java
+++ src/main/java/kr/co/greenuniv/controller/CommunityController.java
@@ -1,53 +1,58 @@
 package kr.co.greenuniv.controller;
 
+import kr.co.greenuniv.dto.PageRequestDTO;
+import kr.co.greenuniv.dto.PageResponseDTO;
+import kr.co.greenuniv.service.CommunityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class CommunityController {
 
-
-    @GetMapping("/community/freeBoardWrite")
-    public String CommunityController() {
-        return "/community/freeBoardWrite";
-    }
+    private final CommunityService communityService;
 
     @GetMapping("/community/CommunityInfo")
-    public String CommunityInfo(){
+    public String CommunityInfo(Model model, PageRequestDTO pageRequestDTO) {
 
+        // 전체 글 조회 서비스 호출(JPA)
+        PageResponseDTO pageResponseDTO = communityService.findAll(pageRequestDTO);
 
+        // 전체 글 조회 서비스 호출(Mybatis)
+        model.addAttribute(pageResponseDTO);
 
         return "/community/CommunityInfo";
     }
 
     @GetMapping("/community/Einformation")
     public String Einformation(){
-
         return "/community/Einformation";
     }
 
     @GetMapping("/community/Fboard")
     public String Fboard(){
-
         return "/community/Fboard";
     }
 
     @GetMapping("/community/news")
     public String news(){
-
         return "/community/news";
     }
 
     @GetMapping("/community/qna")
     public String qna(){
-
         return "/community/qna";
     }
 
     @GetMapping("/community/reference")
     public String reference(){
-
         return "/community/reference";
+    }
 
+    @GetMapping("/community/newsView")
+    public String newsView() {
+        return "/community/newsView";
     }
 }
